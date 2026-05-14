79b6f83dd7304a833010ea077f050e1bedc3ae55
EdsonTena777 <edsontena@outlook.com>
2026-05-13 22:47:14 -0600

    Mejora de errores

--- src/main/java/com/taskmanager/api/Exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/taskmanager/api/Exceptions/GlobalExceptionHandler.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException(
-            RuntimeException ex){
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFound(
+            TaskNotFoundException ex){
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
